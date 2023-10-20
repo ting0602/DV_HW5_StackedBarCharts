3e222129e3d6b8bac403a59d2165442919448d46
ting0602 <ting0602.cs09@nycu.edu.tw>
2023-10-20 16:26:35 +0800

    feat: done

--- script.js
+++ script.js
@@ -1,217 +1,198 @@
-// load iris.csv
-d3.csv("http://vis.lab.djosix.com:2023/data/iris.csv").then(function(data) {
-    // init setting
-    data = data.filter(function(d) {
-        return d['class'] !== "";
-    });
-
-    const svg = d3.select("#plot")
-        .attr("width", 800)
-        .attr("height", 700);
-    const margin = { top: 50, right: 50, bottom: 50, left: 100 };
-    const width = +svg.attr("width") - margin.left - margin.right;
-    const height = +svg.attr("height") - margin.top - margin.bottom;
-
-    keys = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
-
-    // mapping class to color
-    const color = d3.scaleOrdinal()
-        .domain(data.map(d => d['class']))
-        .range(["red", "green", "blue"]);
-
-    const features = ["sepal length", "sepal width", "petal length", "petal width"];
-    const cellSize = 330;
-    const padding = 15; // Padding between cells
-    const dotSize = 3;
-    let plot = null;
-    let clickedColor = null;
-    let isClicked = false;
-
-    features.forEach(function (xFeature, xIndex) {
-        features.forEach(function (yFeature, yIndex) {
-            const xScale = d3.scaleLinear()
-                .domain([d3.min(data, d => +d[xFeature]), d3.max(data, d => +d[xFeature])])
-                .range([padding, cellSize - padding]); // Include padding
-            const yScale = d3.scaleLinear()
-                .domain([d3.min(data, d => +d[yFeature]), d3.max(data, d => +d[yFeature])])
-                .range([cellSize - padding, padding]); // Include padding
-            const hist_yScale = d3.scaleLinear()
-                .domain([0, data.length])
-                .range([cellSize - padding, padding]); // Include padding
-    
-            const xAxis = d3.axisBottom(xScale);
-            const yAxis = d3.axisLeft(yScale);
-            const hist_yAxis = d3.axisLeft(hist_yScale);
-                
-            plot = svg.append("g")
-                .attr("class", "plot")
-                .attr("transform", `translate(${xIndex * (cellSize + padding)+50}, ${yIndex * (cellSize + padding)})`);
-    
-            if (xIndex === yIndex) {
-                const barWidth = (cellSize - padding*2) / 10
-
-                // mapping function
-                const mapToCategory = d3.scaleQuantize()
-                    .domain([d3.min(data, d => +d[xFeature]), d3.max(data, d => +d[xFeature])])
-                    .range(d3.range(10));
-
-                // init
-                const categoryCounts = Array.from({ length: 10 }, () => ({
-                    'Iris-setosa': 0,
-                    'Iris-versicolor': 0,
-                    'Iris-virginica': 0
-                }));
-                
-                // Grouping by xFeature
-                data.forEach(d => {
-                    const xValue = +d[xFeature];
-                    const categoryIndex = mapToCategory(xValue);
-                    const className = d.class;
-                    categoryCounts[categoryIndex][className]++;
-                });
-
-                const stack = d3.stack()
-                .keys(keys)
-                .order(d3.stackOrderNone)
-                .offset(d3.stackOffsetNone);
-                
-                const stackedData = stack(categoryCounts);
-
-                // barGroups
-                const barGroups = plot.selectAll(".bar-group")
-                    .data(stackedData)
-                    .enter()
-                    .append("g")
-                    .attr("class", "bar-group")
-                    .style("fill", d => {
-                        return color(d.key);
-                    });
-                
-                // stack
-                barGroups.selectAll("rect")
-                    .data(d => d)
-                    .enter()
-                    .append("rect")
-                    .attr("x", (d, i) => (i+0.5) * barWidth)
-                    .attr("y", d => hist_yScale(d[1]))
-                    .attr("width", barWidth)
-                    .attr("height", d => hist_yScale(d[0]) - hist_yScale(d[1]))
-
-            } else {
-                // Create a scatter plot
-                plot.selectAll(".dot")
-                    .data(data.filter(d => d[xFeature] != 0))
-                    .enter().append("circle")
-                    .attr("class", "dot")
-                    .attr("cx", d => xScale(+d[xFeature]))
-                    .attr("cy", d => yScale(+d[yFeature]))
-                    .attr("r", dotSize)
-                    .style("fill", d => color(d['class']))
-                    .on("mouseover", handleMouseOver) // show up the data info
-                    .on("mouseout", handleMouseOut) // fade away
-                    .on("click", handleMouseClick); // click
-            }
-    
-            // Add x-axis labels
-            plot.append("text")
-                .attr("class", "x-axis-label")
-                .attr("x", cellSize / 2)
-                .attr("y", cellSize + padding)
-                .style("text-anchor", "middle")
-                .text(xFeature);
-    
-            // Add x-axis
-            plot.append("g")
-                .attr("class", "x-axis")
-                .attr("transform", `translate(0, ${cellSize - padding})`)
-                .call(xAxis);
-
-            if (xIndex !== yIndex) {
-                // Add y-axis labels
-                plot.append("text")
-                .attr("class", "y-axis-label")
-                .attr("x", -(cellSize / 2)) // Move the y-label to the left
-                .attr("y", -padding)
-                .style("text-anchor", "middle")
-                .attr("transform", "rotate(-90)")
-                .text(yFeature);
-
-                // Add y-axis
-                plot.append("g")
-                .attr("class", "y-axis")
-                .attr("transform", `translate(${padding}, 0)`)
-                .call(yAxis);
-            } else {
-                plot.append("g")
-                .attr("class", "y-axis")
-                .attr("transform", `translate(${padding}, 0)`)
-                .call(hist_yAxis);
-            }
+d3.csv("http://vis.lab.djosix.com:2023/data/TIMES_WorldUniversityRankings_2024.csv")
+    .then(function(data) {
+        data = data.filter(function(d) {
+            return d['rank'] !== "";
         });
-    });
 
-    function handleMouseClick(event, d) {
-        const currentColor = d3.select(this).style('fill');
-        const className = d['class'];
+        const svgWidth = 2000, svgHeight = 1100;
+        const margin = {top: 35, right: 500, bottom: 700, left: 200};
+        const width = svgWidth - margin.left - margin.right;
+        const height = svgHeight - margin.top - margin.bottom;
+
+        // init svg
+        const svg = d3.select("body").append("svg")
+            .attr("width", svgWidth)
+            .attr("height", svgHeight)
+            .append("g")
+            .attr("transform", `translate(${margin.left},${margin.top})`);
+
+        const complete_data = data.slice(0, 1904);
+        let ascending = false;
+        let numSchools = 100;
+        let selectedCriteria = ["scores_teaching", "scores_research", "scores_citations", "scores_industry_income", "scores_international_outlook"];
+        const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+        d3.select("#sortOrder").on("change", function() {
+            ascending = document.getElementById("sortOrder").value === "ascending";
+            updateChart(complete_data, ascending, numSchools, selectedCriteria);
+        });
+
+        d3.select("#schoolNum").on("input", function() {
+            numSchools = Number(document.getElementById("schoolNum").value);
+            updateChart(complete_data, ascending, numSchools, selectedCriteria);
+        });
+        d3.selectAll("input[name='criteria']").on("change", function() {
+            selectedCriteria = Array.from(document.querySelectorAll("input[name='criteria']:checked")).map(input => input.value);
+            updateChart(complete_data, ascending, numSchools, selectedCriteria);
+        });
         
-        if (isClicked && clickedColor === currentColor) {
-          // recover
-          svg.selectAll(".dot")
-            .transition()
-            .duration(500)
-            .style("opacity", 1)
-            .attr("r", dotSize);
-          clickedColor = null;
-          isClicked = false;
-        } else {
-          // enlarge && opacity:0.5
-          svg.selectAll(".dot")
-            .transition()
-            .duration(500)
-            .style("opacity", d => d['class'] === className ? 1 : 0.5)
-            .attr("r", d => d['class'] === className ? dotSize * 1.5 : dotSize);
-          clickedColor = currentColor;
-          isClicked = true;
+        updateChart(complete_data, ascending, numSchools, selectedCriteria);
+
+        function updateChart(complete_data, ascending, numSchools, selectedCriteria) {
+            svg.selectAll(".series").remove();
+            svg.selectAll(".legend").remove();
+            svg.selectAll(".x-axis").remove();
+            svg.selectAll(".y-axis").remove();
+            
+            const data = complete_data.slice(0, numSchools);
+
+            data.forEach(d => {
+                let total = 0
+                selectedCriteria.forEach(criterion => {
+                    total += +d[criterion]
+                });
+                d.total = total
+            });
+
+            if (ascending) {
+                data.sort((a, b) => d3.ascending(a.total, b.total))
+            } else {
+                data.sort((a, b) => d3.descending(a.total, b.total))
+            }
+            let stackedData = d3.stack()
+                .keys(selectedCriteria)
+                (data);
+
+
+            const xScale = d3.scaleBand()
+                .domain(data.map(d => d.name))
+                .range([0, width])
+                .padding(0.1);
+
+            const yScale = d3.scaleLinear()
+                .domain([0, d3.max(stackedData, d => d3.max(d, d => d[1]))])
+                .range([height, 0]);
+
+            // stack bar chart
+            svg.selectAll(".series")
+                .data(stackedData)
+                .enter().append("g")
+                    .attr("class", "series")
+                    .attr("fill", (d, i) => colorScale(i))
+                    .selectAll("rect")
+                    .data(d => d)
+                    .enter().append("rect")
+                        .attr("x", (d, i) => xScale(data[i].name))
+                        .attr("y", d => yScale(d[1]))
+                        .attr("height", d => yScale(d[0]) - yScale(d[1]))
+                        .attr("width", xScale.bandwidth())
+                        .on("mouseover", handleMouseOver)
+                        .on("mouseout", handleMouseOut); // fade away
+
+            // draw x, y-axis
+            svg.append("g")
+                .attr("class", "x-axis")
+                .attr("transform", `translate(0,${height})`)
+                .call(d3.axisBottom(xScale))
+                .selectAll("text")
+                .style("text-anchor", "end")
+                .attr("transform", "rotate(-45)");
+
+            svg.append("g")
+                .attr("class", "y-axis")
+                .call(d3.axisLeft(yScale).ticks(10));
+
+            // School legend
+            const legend = svg.selectAll(".legend")
+                .data(selectedCriteria)
+                .enter().append("g")
+                    .attr("class", "legend")
+                    .attr("transform", (d, i) => "translate(1350," + (20 * i) + ")");
+
+            legend.append("rect")
+                .attr("x", 0)
+                .attr("width", 18)
+                .attr("height", 18)
+                .attr("fill", (d, i) => colorScale(i));
+
+            legend.append("text")
+                .attr("x", 25)
+                .attr("y", 9)
+                .attr("dy", ".35em")
+                .style("text-anchor", "start")
+                .text(d => d);
         }
-      }
-})
 
-// Additional Functions
-// MouseOver => show info
-function handleMouseOver(event, d) {
-    // get info
-    const sepalLength = d['sepal length'];
-    const sepalWidth = d['sepal width'];
-    const petalLength = d['petal length'];
-    const petalWidth = d['petal width'];
-    const flowerClass = d['class'];
 
-    // create info box
-    const tooltip = d3.select("body")
-        .append("div")
-        .attr("class", "tooltip")
-        .style("position", "absolute")
-        .style("background-color", "white")
-        .style("border", "1px solid #ddd")
-        .style("padding", "10px")
-        .style("opacity", 0.9);
+        function handleMouseOver(event, d) {
+            // get info
+            const name = d.data['name'];
+            const rank = d.data['rank'];
+            const scores_teaching_rank = d.data['scores_teaching_rank'];
+            const scores_teaching = +d.data['scores_teaching'];
+            const scores_research_rank = d.data['scores_research_rank'];
+            const scores_research = +d.data['scores_research'];
+            const scores_citations_rank = d.data['scores_citations_rank'];
+            const scores_citations = +d.data['scores_citations'];
+            const scores_industry_income_rank = d.data['scores_industry_income_rank'];
+            const scores_industry_income = +d.data['scores_industry_income'];
+            const scores_international_outlook_rank = d.data['scores_international_outlook_rank'];
+            const scores_international_outlook = +d.data['scores_international_outlook'];
+            const location = d.data['location'];
 
-    // info box HTML
-    tooltip.html(`
-        <p>Sepal Length: ${sepalLength}</p>
-        <p>Sepal Width: ${sepalWidth}</p>
-        <p>Petal Length: ${petalLength}</p>
-        <p>Petal Width: ${petalWidth}</p>
-        <p>Class: ${flowerClass}</p>
-    `);
+            // create info box
+            const tooltip = d3.select("body")
+                .append("div")
+                .attr("class", "tooltip")
+                .style("position", "absolute")
+                .style("background-color", "white")
+                .style("border", "1px solid #ddd")
+                .style("padding", "10px")
+                .style("opacity", 0.9);
 
-    // info box CSS
-    tooltip.style("left", (event.pageX + 10) + "px")
-        .style("top", (event.pageY + 10) + "px");
+            // info box HTML
+            tooltip.html(`
+                <h3>${name}</h3>
+                <p>rank: ${rank}</p>
+                <p>total score: ${(scores_teaching + scores_research + scores_citations + scores_industry_income + scores_international_outlook).toFixed(1)}</p>
+                <p>scores teaching rank / score: ${scores_teaching_rank} / ${scores_teaching}</p>
+                <p>scores research rank / score: ${scores_research_rank} / ${scores_research}</p>
+                <p>scores citations rank / score: ${scores_citations_rank} / ${scores_citations}</p>
+                <p>scores industry_income rank / score: ${scores_industry_income_rank} / ${scores_industry_income}</p>
+                <p>scores international_outlook rank / score: ${scores_international_outlook_rank} / ${scores_international_outlook}</p>
+                <p>location: ${location}</p>
+            `);
 
-}
+            // info box CSS
+            tooltip.style("left", (event.pageX + 10) + "px")
+                .style("top", (event.pageY + 10) + "px");
 
-// MouseOut => fade away
-function handleMouseOut() {
-    d3.select(".tooltip").remove();
-}
+
+            d3.select(event.currentTarget)
+                .transition()
+                .duration(300)
+                .attr("opacity", 0.5);
+
+
+            var svgBounds = svg.node().getBoundingClientRect();
+            var xPosition = event.clientX - svgBounds.left - (margin.left/2);
+            var yPosition = event.clientY - svgBounds.top;
+            svg.append("text")
+                .attr("class", "info")
+                .attr("x", xPosition)
+                .attr("y", yPosition - 10)
+                .style("text-anchor", "start")
+                .text(`${(d[1] - d[0]).toFixed(1)}`);
+            console.log( event.pageX ,  event.pageY)
+        }
+
+        // MouseOut => fade away
+        function handleMouseOut(event, d) {
+            d3.select(".tooltip").remove();
+
+            d3.select(event.currentTarget)
+                .transition()
+                .duration(300)
+                .attr("opacity", 1);
+            svg.selectAll(".info").remove();
+        }
+    });
